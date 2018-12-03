85805656601b7be3763b9bdb84f4b02e999c36b6
Jan Mikeš <j.mikes@me.com>
2018-12-03 22:31:38 +0100

    Moved UserTest - tweaked namespace

--- tests/Domain/UserTest.php
+++ tests/Domain/UserTest.php
@@ -1,6 +1,6 @@
 <?php declare (strict_types=1);
 
-namespace Caloriary\Tests\Domain\User;
+namespace Caloriary\Tests\Domain;
 
 use Caloriary\Domain\User;
 use Caloriary\Domain\Value\ClearTextPassword;
