34bdb079128213aaa173b5dc14aaaf4fea38211e
Jan Mikeš <j.mikes@me.com>
2018-12-04 02:42:50 +0100

    Added dummy API endpoint actions

---
+++ src/Infrastructure/API/Action/RegisterUserAction.php
@@ -0,0 +1,15 @@
+<?php declare (strict_types=1);
+
+namespace Caloriary\Infrastructure\API\Action;
+
+use BrandEmbassy\Slim\ActionHandler;
+use BrandEmbassy\Slim\Request\RequestInterface;
+use BrandEmbassy\Slim\Response\ResponseInterface;
+
+final class RegisterUserAction implements ActionHandler
+{
+	public function __invoke(RequestInterface $request, ResponseInterface $response, array $arguments = []): ResponseInterface
+	{
+		return $response->withJson([], 201);
+	}
+}
