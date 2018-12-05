a28ebcaf9727a2338df66f8b6b24eabf5bae67f7
Jan Mikeš <j.mikes@me.com>
2018-12-05 22:54:53 +0100

    Working Login endpoint

--- src/Application/Action/LoginAction.php
+++ src/Application/Action/LoginAction.php
@@ -5,11 +5,71 @@ namespace Caloriary\Application\Action;
 use BrandEmbassy\Slim\ActionHandler;
 use BrandEmbassy\Slim\Request\RequestInterface;
 use BrandEmbassy\Slim\Response\ResponseInterface;
+use Caloriary\Authentication\Exception\AuthenticationFailed;
+use Caloriary\Authentication\Exception\UserNotFound;
+use Caloriary\Authentication\Repository\Users;
+use Caloriary\Authentication\Value\ClearTextPassword;
+use Caloriary\Authentication\Value\EmailAddress;
+use Caloriary\Infrastructure\Application\Response\ResponseFormatter;
 
 final class LoginAction implements ActionHandler
 {
+	/**
+	 * @var Users
+	 */
+	private $users;
+
+	/**
+	 * @var ResponseFormatter
+	 */
+	private $responseFormatter;
+
+
+	public function __construct(
+		Users $users,
+		ResponseFormatter $responseFormatter
+	)
+	{
+		$this->users = $users;
+		$this->responseFormatter = $responseFormatter;
+	}
+
+
 	public function __invoke(RequestInterface $request, ResponseInterface $response, array $arguments = []): ResponseInterface
 	{
-		return $response->withJson([], 201);
+		$body = $request->getDecodedJsonFromBody();
+
+		// @TODO: Validate body, via middleware?
+		// @TODO: Transform into DTO, so we have strict types
+
+		try {
+			$emailAddress = EmailAddress::fromString($body->email ?? '');
+			$password = ClearTextPassword::fromString($body->password ?? '');
+			$user = $this->users->get($emailAddress);
+
+			$user->authenticate($password);
+		}
+
+		catch (\InvalidArgumentException $e) {
+			return $this->responseFormatter->formatError($response, $e->getMessage());
+		}
+
+		catch (UserNotFound $e) {
+			return $this->createAuthenticationFailedResponse($response);
+		}
+
+		catch (AuthenticationFailed $e) {
+			return $this->createAuthenticationFailedResponse($response);
+		}
+
+		return $response->withJson([
+			'success' => true,
+		], 201);
+	}
+
+
+	private function createAuthenticationFailedResponse(ResponseInterface $response): ResponseInterface
+	{
+		return $this->responseFormatter->formatError($response, 'Authentication failed - Invalid combination of credentials', 401);
 	}
 }
