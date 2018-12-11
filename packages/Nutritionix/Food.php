2e12eda2c1049fbf1ceac10303c178849a75823f
Jan Mikeš <j.mikes@me.com>
2018-12-11 03:37:17 +0100

    First draft of nutritionix library

---
+++ packages/Nutritionix/Food.php
@@ -0,0 +1,35 @@
+<?php declare (strict_types=1);
+
+namespace JanMikes\Nutritionix;
+
+final class Food
+{
+	/**
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @var int
+	 */
+	private $calories;
+
+
+	public function __construct(string $name, int $calories)
+	{
+		$this->name = $name;
+		$this->calories = $calories;
+	}
+
+
+	public function name(): string
+	{
+		return $this->name;
+	}
+
+
+	public function calories(): int
+	{
+		return $this->calories;
+	}
+}
--- packages/Nutritionix/Nutritionix.php
+++ packages/Nutritionix/Nutritionix.php
@@ -2,9 +2,48 @@
 
 namespace JanMikes\Nutritionix;
 
+use GuzzleHttp\Client;
+use Nette\Utils\Json;
+
 final class Nutritionix
 {
+	/**
+	 * @var Client
+	 */
+	private $client;
+
+
 	public function __construct(string $applicationId, string $apiKey)
 	{
+		$this->client = new Client([
+			'base_uri' => 'https://trackapi.nutritionix.com/',
+			'headers' => [
+				'x-app-id' => $applicationId,
+				'x-app-key' => $apiKey,
+				'x-remote-user-id' => 0,
+				'content-type' => 'application/json',
+				'accept' => 'application/json',
+			],
+		]);
+	}
+
+
+	/**
+	 * @return Food[]
+	 */
+	public function searchForFoods(string $text): array
+	{
+		$response = $this->client->post('/v2/natural/nutrients', [
+			'body' => [
+				'query' => $text,
+			],
+		]);
+
+		$body = (string) $response->getBody();
+		$data = Json::decode($body);
+
+		return array_map(function(\stdClass $food) {
+			return new Food($food->food_name, $food->nf_calories);
+		}, $data->foods);
 	}
 }
