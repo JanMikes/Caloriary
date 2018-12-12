70691cfecf4e6a8c89b08b14a7c1787b7a30bdec
Jan Mikeš <j.mikes@me.com>
2018-12-12 12:49:35 +0100

    Applied filtering to COuntUsers

--- src/Infrastructure/Authentication/ReadModel/DQLCountUsers.php
+++ src/Infrastructure/Authentication/ReadModel/DQLCountUsers.php
@@ -2,16 +2,25 @@
 
 namespace Caloriary\Infrastructure\Authentication\ReadModel;
 
+use Caloriary\Application\Filtering\Exception\InvalidFilterQuery;
+use Caloriary\Application\Filtering\FilteringAwareQuery;
+use Caloriary\Application\Filtering\QueryFilters;
 use Caloriary\Authentication\ReadModel\CountUsers;
 use Caloriary\Authentication\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\QueryException;
 
-final class DQLCountUsers implements CountUsers
+final class DQLCountUsers implements CountUsers, FilteringAwareQuery
 {
 	/**
 	 * @var EntityManagerInterface
 	 */
 	private $entityManager;
+
+	/**
+	 * @var QueryFilters|null
+	 */
+	private $filters;
 
 
 	public function __construct(EntityManagerInterface $entityManager)
@@ -22,11 +31,31 @@ final class DQLCountUsers implements CountUsers
 
 	public function __invoke(): int
 	{
-		return (int) $this->entityManager->createQueryBuilder()
+		$builder = $this->entityManager->createQueryBuilder()
 			->from(User::class, 'user')
-			->select('COUNT(user.emailAddress)')
-			->getQuery()
-			->setMaxResults(1)
-			->getSingleScalarResult();
+			->select('COUNT(user.emailAddress)');
+
+		try {
+			if ($this->filters) {
+				$builder->andWhere($this->filters->dql());
+				$builder->setParameters($this->filters->parameters());
+
+				// This should prevent bugs, filters will be valid only for next Query
+				$this->filters = null;
+			}
+
+			return (int) $builder
+				->getQuery()
+				->setMaxResults(1)
+				->getSingleScalarResult();
+		} catch (QueryException $e) {
+			throw InvalidFilterQuery::fromQueryException($e);
+		}
+	}
+
+
+	public function applyFiltersForNextQuery(QueryFilters $filters): void
+	{
+		$this->filters = $filters;
 	}
 }
